b211a365bc031dca6935693339599c1a73a41efb
Yana Vitkovskaya <vitkovskaya.yana@gmail.com>
2022-11-12 18:29:09 +0300

    Add classes and objects

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,59 @@
+package ru.netology.radio;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        } else {
+            currentVolume = currentVolume;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else {
+            currentVolume = currentVolume;
+        }
+    }
+
+    public void next() {
+        if (currentStation < 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+}
+
